3eed24a93433f4aca37f1907e1262c455ea059f8
achiewe <mamukashviliachi706@gmail.com>
2025-01-17 13:50:25 +0400

    create some schema to define structure and inside the project-conroller file reate async function to show this recieve information

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import connect from "./database/mongo.js";
 import dotenv from "dotenv";
+import { getAllProjects } from "./controllers/project-controller.js";
 
 dotenv.config();
 
@@ -11,5 +12,7 @@ const app = express();
 app.get("/", (req, res) => {
   return res.status(200).json({ message: "app works!" });
 });
+
+app.get("/api/projects", getAllProjects);
 
 app.listen(4000);
