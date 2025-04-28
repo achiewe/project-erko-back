886f91ce0ec74e5034799c2466336a7af5bb9b20
achiewe <mamukashviliachi706@gmail.com>
2025-04-28 17:51:11 +0400

    change to upload more file size

--- src/app.js
+++ src/app.js
@@ -16,8 +16,8 @@ connect();
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ extended: true, limit: "50mb" }));
 
 cloudinary.v2.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
@@ -27,7 +27,10 @@ cloudinary.v2.config({
 
 // Multer Configuration (store files in memory)
 const storage = multer.memoryStorage();
-const upload = multer({ storage });
+const upload = multer({
+  storage,
+  limits: { fileSize: 50 * 1024 * 1024 }, // 50MB limit
+});
 
 // Upload Buffer to Cloudinary Function
 const uploadToCloudinary = (buffer) => {
